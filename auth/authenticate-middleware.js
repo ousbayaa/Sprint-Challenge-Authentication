13ab04cf1d12ee74aef2cce3c45852f9986f808c
Ous Bayaa <bayaaous@gmail.com>
2020-04-24 09:51:10 -0700

    authenticator is working

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -2,11 +2,28 @@
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
+const jwt = require('jsonwebtoken'); // ---> npm i jsonwebtoken
+const secrets = require('../api/secrets');
 
 module.exports = (req, res, next) => {
-  if (req.session && req.session.name) {
-    next();
-  } else {
-    res.status(401).json({ message: "You shall not pass!!" });
-  }
+  // tokens are normally sent as the Authorisation header
+  const token = req.headers.authorization;
+
+  const secret = secrets.jwtSecret;
+  
+  if(token) {    
+    // verify that the token is valid
+    jwt.verify(token, secret, (error, decodedToken) => {
+      // if everything is good with the token, the error will be undefined
+      if(error) {
+        res.status(401).json({you: 'shall not pass!'});
+      } else {
+        req.decodedToken = decodedToken;
+        
+        next();
+      }
+    });
+      } else {
+        res.status(400).json({message: 'Please provide credentials'})
+      }
 };
